fba9c316ca79ae6ef0bb90600dec7cc7b9d5951a
Pedro Robson Leao <pedro.leao@gmail.com>
2014-08-14 21:25:51 -0300

    match Date LogInfo Exception

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,78 +1,86 @@
 import scala.io._
 import scala.util.matching.Regex
+import java.text.SimpleDateFormat
 
 class Patterns {
   val patterns = Map(
     "Default" -> new Regex(".*")
   )
-  
-  def getPatterns() : Map[String,Regex] = {
-    return patterns
+
+  def getPatterns(): Map[String, Regex] = {
+    patterns
   }
 }
 
 class Date() extends Patterns() {
   override val patterns = Map(
-    "YYYY-mm-dd HH:MM:SS" -> new Regex("[2-9][0-9]{3}-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]"),
-    "MMM dd, YYYY H:MM:SS aaa" -> new Regex("[A-Z][a-z]{2} [0-3][0-9], [2-9][0-9]{3} [0-9]{1,2}:[0-5][0-9]:[0-5][0-9] [AP]M")
-  ) 
+    "yyyy-MM-dd HH:mm:ss" -> new Regex("[2-9][0-9]{3}-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]"),
+    "MMM dd, yyyy H:mm:ss a" -> new Regex("[A-Z][a-z]{2} [0-3][0-9], [2-9][0-9]{3} [0-9]{1,2}:[0-5][0-9]:[0-5][0-9] [AP]M"),
+    "M/dd/yy H:mm:ss:SSS" -> new Regex("[0-9]{1,2}/[0-3][0-9]/[0-9]{2} [0-9]{1,2}:[0-5][0-9]:[0-5][0-9]:[0-9]{3}")
+  )
 }
 
-class Exception()  extends Patterns() {
+class Exception() extends Patterns() {
   override val patterns = Map(
     "Exception" -> new Regex("[a-zA-Z.]+Exception")
   )
 }
+
+class LogInfo() extends Patterns() {
+  override val patterns = Map(
+    "Info" -> new Regex(" (ERROR|WARN|E|W) ")
+  )
+}
+
 object Test {
-  
+
   def main(args: Array[String]) {
 
-    var line = ""
+    val separator = ","
+    val date = new Date()
+    val info = new LogInfo()
+    val exception = new Exception()
 
-    var date = new Date()
-    var exception = new Exception()
-
-    var lst = List[String]()
     var lnDt = ""
     var lnExc = ""
-        
-    for (line <- Source.stdin.getLines) {
-        
-      date.getPatterns().keys.foreach { 
-        k => 
-        
-        var l = (date.getPatterns()(k) findAllIn line).mkString("|")
-        
-        if(l.length>0) {
-          //	println(l + " Match With:" + date.getPatterns()(k) + " Date Formater:" + k )
-          lnDt = l
-	}
-      }   
-      
-      exception.getPatterns().keys.foreach { 
-        k => 
-        
-        var l = (exception.getPatterns()(k) findAllIn line).mkString(",")
-        
-        if(l.length>0) {
-          //	println(l + " Match With:" + exception.getPatterns()(k) + " Exception Formater:" + k )
-          lnExc = l
-	}
+    var lnInf = ""
 
-	if((lnDt.length>0) {
-            if(lnExc.length>0)) {
-              println(lnDt + " " + lnExc)
-              lnDt = ""
-              lnExc = ""
-            }
+    for (line <- Source.stdin.getLines) {
+
+      info.getPatterns().keys.foreach {
+        k =>
+          val l = (info.getPatterns()(k) findAllIn line).mkString(separator)
+
+          if (l.length > 0) {
+            lnInf = l
           }
-           /*
-            if(lst.length >= 2) {
-            println(lst.mkString(","));
-            lst = List()
-            }
-            */
-           }
-           }
-           }
-           }
+      }
+      date.getPatterns().keys.foreach {
+        k =>
+
+          val l = (date.getPatterns()(k) findAllIn line).mkString(separator)
+
+          if (l.length > 0) {
+            val fmt = new SimpleDateFormat(k)
+            lnDt = fmt.parse(l).toString()
+          }
+      }
+
+      exception.getPatterns().keys.foreach {
+        k =>
+
+          val l = (exception.getPatterns()(k) findAllIn line).mkString(separator)
+
+          if (l.length > 0) {
+            lnExc = l
+          }
+
+          if ((lnDt.length > 0) && (lnExc.length > 0)) {
+            println(lnDt + " " + lnInf + " " + lnExc)
+            lnDt = ""
+            lnExc = ""
+          }
+      }
+    }
+  }
+}
